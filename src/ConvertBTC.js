20d15a07236170a278b69811467c9ec93f857572
Gregory Menezes <gregoryfontenele@gmail.com>
2018-04-19 20:48:57 -0300

    Add loader

--- src/ConvertBTC.js
+++ src/ConvertBTC.js
@@ -1,11 +1,19 @@
 const request = require('request');
 const chalk = require('chalk');
+const ora = require('ora');
+
+const spinner = ora({
+  text: 'Getting bitcoin data..',
+  color: 'green',
+});
 
 function convertBTC(currency = 'BRL', amount = 1) {
   const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
+
+  spinner.start();
   request(url, (error, response, body) => {
     let apiResponse;
-
+    spinner.stop();
     try {
       apiResponse = JSON.parse(body);
     } catch (err) {
